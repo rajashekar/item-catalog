4092edff2646d3c83c33de0e1fb8658a0a85b591
Rajashekar Chintalapati <rchintalapati@walmartlabs.com>
2018-04-08 19:06:34 -0700

    integrating google api - login & logout

--- catalog_views.py
+++ catalog_views.py
@@ -1,4 +1,16 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response, flash, redirect
+from flask import session as login_session
+from oauth2client.client import flow_from_clientsecrets, FlowExchangeError
+
+import httplib2
+import random
+import string
+import json
+import requests
+import ptvsd
+
+CLIENT_ID = json.loads(open('client_secret_google.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Item Catalog App"    
 
 app = Flask(__name__)
 
@@ -6,6 +18,102 @@ app = Flask(__name__)
 def showCatalog():
     return render_template('catalog.html')
 
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    login_session['state'] = state
+    return render_template('login.html', STATE=state, session=login_session)
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # if token is not valid, return error
+    print "request args :", request.args
+    print "login session ",login_session
+    if request.args.get('state') != login_session['state']:
+        return sendreponse('Invalid state parameter.',401)
+    # get the code
+    code = request.data
+
+    try:
+        oauth_flow = flow_from_clientsecrets('client_secret_google.json',scope='')
+        print "here 1"
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        return sendreponse('Failed to upgrade the authorization code.',401)
+
+    # check that the access token is valid
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    h = httplib2.Http() 
+    result = json.loads(h.request(url, 'GET')[1]) 
+    if result.get('error') is not None:
+        return sendreponse(result.get('error'),500)
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']     
+    if result['user_id'] != gplus_id:
+        return sendreponse("Token's user ID does'nt match given user ID.",401)
+
+    #Verify that the access token is valid for this app
+    if result['issued_to'] != CLIENT_ID:
+        return sendreponse("Token's client ID does'nt match.",401)
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+    login_session['username'] = data['name']  
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+    login_session['provider'] = 'google'
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;">'
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
+
+@app.route('/disconnect')
+def disconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print 'Acess Token is None'
+        return sendreponse('Current user not connected', 401)
+    print 'In disconnect access token is %s', access_token
+    print 'User name is: ', login_session['username']
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print 'result is', result
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        print 'Successfully disconnected'
+        return redirect('/')
+    else: 
+        return sendreponse('Failed to revoke token for given user.',400)    
+
+def sendreponse(message, statusCode):
+    response = make_response(json.dumps(message),statusCode)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
